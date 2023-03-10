172c27229c7e7df19fde3dbe3a6dec67399ea946
andrey <andrey.kozhevnikov@devexpress.com>
2023-03-10 16:40:15 +0400

    add xpo

---
+++ CS/XPO/MySolution/MySolution.Module/Controllers/CustomControllers.cs
@@ -0,0 +1,14 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.SystemModule;
+
+namespace dxTestSolution.Module.Controllers {
+    public class CustomController : ViewController {
+        protected override void OnActivated() {
+            base.OnActivated();
+            ListViewProcessCurrentObjectController targetController = Frame.GetController<ListViewProcessCurrentObjectController>();
+            if (targetController != null) {
+                targetController.ProcessCurrentObjectAction.Enabled["MyReason"] = false;
+            }
+        }
+    }
+}
